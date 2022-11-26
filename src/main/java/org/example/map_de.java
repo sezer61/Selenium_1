687f9f497af4c1793578b3bf603eb5301eeb6325
hakankob0@gmail.com <sezer.0734@hotmail.com>
2022-11-27 01:21:04 +0300

    S

--- src/main/java/org/example/map_de.java
+++ src/main/java/org/example/map_de.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 public class map_de {
     public static void main(String[] args) {
-        Map<Integer,String> ogrenciMap= Maps_4.Map_Olustur();
-        System.out.println(ogrenciMap);
+
     }
 }
