575cd4ecf6bf62634161fe1ae4a778a54bba175a
hakankob0@gmail.com <sezer.0734@hotmail.com>
2022-11-28 01:43:56 +0300

    S

--- src/main/java/org/example/Fazlayı_sil.java
+++ src/main/java/org/example/Fazlayı_sil.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class Fazlayı_sil {
     public static void main(String[] args) {
         Map<Integer,String> ogrenciMapi=Map_Depo.Map_olustur();
-        ogrenciMapi.put(109,"Adem-demier-11-d-soz");
+        ogrenciMapi.put(109,"Adem-demier-11-Manav_sorusu-soz");
 
         Set<String>siraliogrencilistesi=Map_Depo.siraliOgrencilistesi(ogrenciMapi);
         System.out.println(siraliogrencilistesi);
---
+++ src/main/java/org/example/Manav_sorusu.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Manav_sorusu {
+        public static void main(String[] args) {
+
+            Map<String, Integer> urunList = new HashMap<>();
+            urunList.put("Un", 140);
+            urunList.put("Şeker", 29);
+            urunList.put("Pirinç", 33);
+            double sonuc = 0;
+            boolean sayi = true;
+            do {
+                Scanner scan = new Scanner(System.in);
+                System.out.println("Lutfen almak istediginiz urun ismini giriniz : " +
+                        "\n->> 1-Un" +
+                        "\n->> 2-Şeker" +
+                        "\n->> 3-Pirinç");
+                int urun = scan.nextInt();
+                switch (urun) {
+                    case 1:
+                        System.out.println("Kaç kilo Un istiyorsunuz : ");
+                        double kilo = scan.nextDouble();
+                        sonuc += urunList.get("Un")*kilo;
+                        System.out.println("Baska bir urun eklemek istiyor musunuz?" +
+                                "\nEvet->> 1" +
+                                "\nHayır->> 2");
+                        String degis = scan.next();
+                        if (degis.equals("2")) {
+                            sayi = false;
+                        } else if (degis.equals("1")) {
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Kaç kilo Şeker istiyorsunuz : ");
+                        double kilo1 = scan.nextDouble();
+                        sonuc += urunList.get("Şeker")*kilo1;
+
+                        System.out.println("Baska bir urun eklemek istiyor musunuz?" +
+                                "\nEvet->> 1" +
+                                "\nHayır->> 2");
+                        String degis0 = scan.next();
+
+                        if (degis0.equals("2")) {
+                            sayi = false;
+                        } else if (degis0.equals("1")) {
+
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Kaç kilo Pirinç istiyorsunuz : ");
+                        double kilo2 = scan.nextDouble();
+                        sonuc += urunList.get("Pirinç")*kilo2;
+
+                        System.out.println("Baska bir urun eklemek istiyor musunuz?" +
+                                "\nEvet->> 1" +
+                                "\nHayır->> 2");
+                        String degis1 = scan.next();
+
+                        if (degis1.equals("2")) {
+                            sayi = false;
+                        } else if (degis1.equals("1")) {
+                        }
+                        break;
+                }
+            } while (!sayi == false);
+            System.out.println("Sepet tutariniz : " + sonuc + " TL");
+        }
+    }
+
